b4e2a4ab6df05475f0d78d0bc462570bd4b59349
Rodrigo Barbosa <rodrigo.silva018@gmail.com>
2024-12-14 19:32:32 -0400

    Projeto Inicial

---
+++ Documents/CURSOS ALURA/logica-js-projeto_inicial/app.js
@@ -0,0 +1,11 @@
+alert('Boas Vindas ao Jogo do Número Secreto');
+let numeroSecreto = 4;
+console.log(numeroSecreto)
+let chute = prompt('Escolha um número entre 1 e 10');
+
+If (chute == numeroSecreto) {
+    alert (`Isso aí! Você descobriu o ${numeroSecreto} 5`);
+}
+else {
+    alert ('Você errou');
+} 
