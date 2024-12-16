e097b51ac89b92191c1eaf1c9b21bda0620eaa06
Rodrigo Barbosa <rodrigo.silva018@gmail.com>
2024-12-15 20:29:20 -0400

    Jogo de adivinhar numero atualizado

--- Documents/CURSOS ALURA/logica-js-projeto_inicial/app.js
+++ Documents/CURSOS ALURA/logica-js-projeto_inicial/app.js
@@ -1,11 +1,38 @@
 alert('Boas Vindas ao Jogo do Número Secreto');
-let numeroSecreto = 4;
-console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10');
+let NumeroMaximo = 500;
+let numeroSecreto = parseInt (Math.random() * NumeroMaximo +1);
+console.log(numeroSecreto);
+let chute; 
+let tentativa = 1;
 
-If (chute == numeroSecreto) {
-    alert (`Isso aí! Você descobriu o ${numeroSecreto} 5`);
+while (chute != numeroSecreto)  {
+  chute = prompt('Escolha um número entre 1 e ' +NumeroMaximo);
+  // se chute for igual ao número secreto
+  if (chute == numeroSecreto) {
+    break;
+   } else {
+      //alert ('Você errou =(');
+      if (chute > numeroSecreto){
+        alert ('O numero secreto eh menor que ' +chute);
+      } else {
+        alert ('O numero secreto eh maior que ' +chute);
+      }
+      tentativa ++;
+  }
 }
-else {
-    alert ('Você errou');
-} 
+
+let palavraTentativa = tentativa > 1 ? 'tentativas' : 'tentativa';
+alert('Isso aí! Você descobriu o número secreto ' + numeroSecreto + ' com ' + tentativa + ' ' + palavraTentativa);
+
+// if (tentativa ==1 ) {
+//   alert('Isso aí! Você descobriu o número secreto ' + numeroSecreto + ' com ' + tentativa + ' tentativa');
+// } else {
+//   alert('Isso aí! Você descobriu o número secreto ' + numeroSecreto + ' com ' + tentativa + ' tentativas');
+// }
+
+
+ 
+    
+
+    
+  
